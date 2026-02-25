5c5f3f8b088cab40551edbd2c4f9a63bd89b0094
Manan patel <mananpatel5348@gmail.com>
2026-02-25 21:58:47 +0530

    Add user display to admin, customer, and seller dashboards; implement login requirement for dashboard views

--- wearweb/wear/views.py
+++ wearweb/wear/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login')
 def customerdashboardview(request):
     return render(request, 'wear/customer_dashboard.html')
 
+@login_required(login_url='login')
 def sellerdashboardview(request):
     return render(request, 'wear/seller_dashboard.html')
 
+@login_required(login_url='login')
 def admindashboardview(request):
     return render(request, 'wear/admin_dashboard.html')
